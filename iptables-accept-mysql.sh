624b937e2c040bdb8fab841c5ecd01061704c778
simplyjarod <simplyjarod@gmail.com>
2017-07-02 18:37:31 +0200

    Update iptables-accept-mysql.sh
    Now you can add all IPs to access port 3306 writing an asterisk (*) as IP. 
    In interactive mode, your IP is suggested only if found/guessed.
    Added an "service iptables save" after applying iptables rules.

--- iptables-accept-mysql.sh
+++ iptables-accept-mysql.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+# ARE YOU ROOT (or sudo)?
+if [[ $EUID -ne 0 ]]; then
+	echo -e "\e[91mERROR: This script must be run as root\e[0m"
+	exit 1
+fi
+
+
 # Si no hay parametro se pide por pantalla:
-if [ -z $1 ]
-then
-	read -r -p "IPv4 to add to Firewall to connect to MySQL remotely: " ip
+if [ -z $1 ]; then
+	remote_ip="$(echo $SSH_CLIENT | cut -f1 -d" ")"
+	if [ -z $remote_ip ]; then
+		echo "Maybe your current IP address is:" $remote_ip
+	fi
+	read -r -p "IPv4 to add to Firewall to connect to port 3306 (* for all IPs): " ip
 else
 	ip=$1
 fi
 
 
-# Se comprueba que es una IPv4 y se agrega a iptables:
-if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
-then
+# All IPs are allowed to connect to port 3306
+if [[ $ip = * ]]; then 
+	iptables -I INPUT -p tcp --dport 3306 -j ACCEPT
+	service iptables save
+	exit 0
+fi
+
+
+# IPv4 check:
+if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 
 	for i in 1 2 3 4; do
 		if [ $(echo "$ip" | cut -d. -f$i) -gt 255 ]; then
@@ -21,6 +38,7 @@ then
 	done
 
 	iptables -I INPUT -p tcp -s $ip --dport 3306 -j ACCEPT
+	service iptables save
 	exit 0
 else
 	echo -e "\e[91mThe introduced IP does not seem to be correct.\e[0m"
