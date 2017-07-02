97983cde50258451b86b185237935c5d9d2539aa
simplyjarod <simplyjarod@gmail.com>
2017-07-02 18:04:50 +0200

    Update iptables-accept-http.sh
    Now you can add all IPs to access port 80 writing an asterisk (*) as IP

--- iptables-accept-http.sh
+++ iptables-accept-http.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+# ARE YOU ROOT (or sudo)?
+if [[ $EUID -ne 0 ]]; then
+	echo -e "\e[91mERROR: This script must be run as root\e[0m"
+	exit 1
+fi
+
+
 # Si no hay parametro se pide por pantalla:
-if [ -z $1 ]
-then
+if [ -z $1 ]; then
 	remote_ip="$(echo $SSH_CLIENT | cut -f1 -d" ")"
 	echo "Maybe your current IP address is:" $remote_ip
-	read -r -p "IPv4 to add to Firewall to connect to HTTP web server: " ip
+	read -r -p "IPv4 to add to Firewall to connect to port 80 (* for all IPs): " ip
 else
 	ip=$1
 fi
 
 
-# Se comprueba que es una IPv4 y se agrega a iptables:
-if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
-then
+# All IPs area allowed to connect to port 80
+if [[ $ip = * ]]; then 
+	iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+	exit 0
+fi
+
+
+# IPv4 check:
+if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 
 	for i in 1 2 3 4; do
 		if [ $(echo "$ip" | cut -d. -f$i) -gt 255 ]; then
