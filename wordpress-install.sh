62648a9638355925bc787d12e80717f29ed046e4
simplyjarod <simplyjarod@gmail.com>
2020-11-04 12:38:45 +0100

    Update wordpress-install.sh
    Custom DB name, DB user and DB pass added

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -9,6 +9,27 @@ else
 	user=$2
 fi
 
+if [ -z $5 ]; then
+	read -r -p "Do you want to set custom DB name, DB user and DB pass? [Y/n]: " custom_db_names
+	custom_db_names=${custom_db_names,,} # tolower
+	
+	if [[ $custom_db_names =~ ^(yes|y)$ ]]
+	then
+		read -r -p "Name of new DataBase: " dbname
+		read -r -p "Name of new DB User: " dbuser
+		read -s -p "New password for DB User $dbuser: " dbpass
+		echo "" # we need a new line after the password
+	else
+		dbname=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+		dbuser=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+		dbpass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+	fi
+else
+	dbname=$3
+	dbuser=$4
+	dbpass=$5
+fi
+
 
 # if path does not exist, we will try to create it
 if [ ! -d "$path" ]; then
@@ -21,7 +42,6 @@ if [ ! -d "$path" ]; then
 	else
 		mkdir -p $path
 	fi
-
 fi
 
 
@@ -33,17 +53,11 @@ rm -f latest.tar.gz
 mv wordpress/* $path/.
 rm -rf wordpress
 
-chmod 000 $path/xmlrpc.php # some basic security
-
-
 chown $user:$user $path # set the proper permissions to the folder itself
 chown -R $user:$user $path/* # set the proper permissions to all the wordpress files
 
 
 # wordpress database installation and configuration:
-dbname=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
-dbuser=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-dbpass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 ./mysql-create-db-and-user.sh $dbname $dbuser $dbpass
 
 sed -i "s|database_name_here|$dbname|g" $path/wp-config-sample.php
@@ -52,5 +66,7 @@ sed -i "s|password_here|$dbpass|g" $path/wp-config-sample.php
 
 mv $path/wp-config-sample.php $path/wp-config.php
 
+# some very basic "security" and cleaning:
+chmod 000 $path/xmlrpc.php
 rm -f $path/readme.html
-rm -f $path/license.txt
+rm -f $path/lic*.txt # license.txt, licencia.txt...
