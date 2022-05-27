535c0d58f4bd0887af54de3d2fad782de43fdfcd
simplyjarod <simplyjarod@gmail.com>
2022-05-27 23:59:37 +0200

    nodejs-install.sh for centos and ubuntu

--- nodejs-install.sh
+++ nodejs-install.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+# More info: https://github.com/nodesource/distributions
+
 # ARE YOU ROOT (or sudo)?
 if [[ $EUID -ne 0 ]]; then
   echo -e "\e[91mERROR: This script must be run as root\e[0m"
   exit 1
 fi
 
+os=$(grep ^ID= /etc/os-release | cut -d "=" -f 2)
+os=${os,,} #tolower
+
 
 ######################################
 echo "***** NODEJS INSTALLATION *****"
 ######################################
-yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
-yum install -y nodejs
 
+if [[ $os =~ "centos" ]]; then # $os contains "centos"
+  yum install -y gcc-c++ make
+  curl -sL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
+  yum install -y nodejs
+
+elif [[ $os =~ "ubuntu" ]]; then # $os contains "ubuntu"
+  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+	apt-get install -y nodejs
+
+else
+  echo -e "\e[91mOS not detected. NodeJS was not installed\e[0m"
+  exit 1
+fi
 
 
 if type -path "iptables" > /dev/null 2>&1; then
