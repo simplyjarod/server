14290781c04b14455b98c08a198ee7d1d0d2917d
simplyjarod <simplyjarod@gmail.com>
2022-10-11 20:53:19 +0200

    mariadb-install ready for Ubuntu

--- mariadb-install.sh
+++ mariadb-install.sh
@@ -6,28 +6,38 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-centos_version=$(rpm -qa \*-release | grep -Ei "oracle|redhat|centos" | sed 's/[^6-8]*//g' | cut -c1)
+# Operative System:
+os=$(grep ^ID= /etc/os-release | cut -d "=" -f 2)
+os=${os,,} #tolower
 
-yum install mariadb-server -y
 
+if [[ $os =~ "centos" ]]; then # $os contains "centos"
 
-#########################
-# CentOS 6 installation #
-#########################
-if [ "$centos_version" -eq 6 ]; then
-
-	chkconfig --levels 235 mysql on
-	service mysql start
-	mysql_secure_installation
-	service mysql restart
-
-###########################
-# CentOS 7/8 installation #
-###########################
-else
+	yum install mariadb-server -y
 	
-	systemctl enable mariadb
-	systemctl start mariadb.service
+	centos_version=$(rpm -qa \*-release | grep -Ei "oracle|redhat|centos" | sed 's/[^6-8]*//g' | cut -c1)
+
+	# CentOS 6 installation
+	if [ "$centos_version" -eq 6 ]; then
+		chkconfig --levels 235 mysql on
+		service mysql start
+		mysql_secure_installation
+		service mysql restart
+
+	# CentOS 7+ installation
+	else
+		systemctl enable mariadb
+		systemctl start mariadb.service
+		mysql_secure_installation
+		systemctl restart mariadb
+	fi
+
+elif [[ $os =~ "ubuntu" ]]; then # $os contains "ubuntu"
+
+	apt install mariadb-server -y
+	
 	mysql_secure_installation
+	systemctl enable mariadb
+	systemctl start mariadb
 	systemctl restart mariadb
 fi
