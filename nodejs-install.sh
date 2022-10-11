84ac7775a2271cef51a7908136cf1b25c0d5fdee
simplyjarod <simplyjarod@gmail.com>
2022-10-11 20:59:11 +0200

    Update nodejs-install with PM2

--- nodejs-install.sh
+++ nodejs-install.sh
@@ -4,8 +4,8 @@
 
 # ARE YOU ROOT (or sudo)?
 if [[ $EUID -ne 0 ]]; then
-  echo -e "\e[91mERROR: This script must be run as root\e[0m"
-  exit 1
+	echo -e "\e[91mERROR: This script must be run as root\e[0m"
+	exit 1
 fi
 
 os=$(grep ^ID= /etc/os-release | cut -d "=" -f 2)
@@ -17,18 +17,24 @@ echo "***** NODEJS INSTALLATION *****"
 ######################################
 
 if [[ $os =~ "centos" ]]; then # $os contains "centos"
-  yum install -y gcc-c++ make
-  curl -sL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
-  yum install -y nodejs
+	yum install -y gcc-c++ make
+	curl -sL https://rpm.nodesource.com/setup_lts.x | sudo -E bash -
+	yum install -y nodejs
 
 elif [[ $os =~ "ubuntu" ]]; then # $os contains "ubuntu"
-  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+	curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 	apt-get install -y nodejs
 
 else
-  echo -e "\e[91mOS not detected. NodeJS was not installed\e[0m"
-  exit 1
+	echo -e "\e[91mOS not detected. NodeJS was not installed\e[0m"
+	exit 1
 fi
+
+
+###################################
+echo "***** PM2 INSTALLATION *****"
+###################################
+npm install pm2@latest -g
 
 
 if type -path "iptables" > /dev/null 2>&1; then
