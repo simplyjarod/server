0fb100485b57f8f83a260aae71090d679b8e914e
Javier Rodríguez <simplyjarod@gmail.com>
2023-09-21 00:39:09 +0200

    nginx custom log format

--- nginx-install.sh
+++ nginx-install.sh
@@ -24,8 +24,9 @@ elif [ -x "$(command -v apt-get)" ]; then
 
 	sed -i '/http {/a\\tclient_max_body_size 100M;\n' /etc/nginx/nginx.conf
 
+	sed -i '/http {/a\\tlog_format custom_access "$remote_addr ($http_x_forwarded_for) [$time_local] req:\\"$request\\" code:\\"$status\\" sentB:\\"$body_bytes_sent\\" ref:\\"$http_referer\\" agent:\\"$http_user_agent\\" host:\\"$host\\"";\n' /etc/nginx/nginx.conf
+
 	sed -i '/http {/a\\tindex index.php index.html index.htm;\n' /etc/nginx/nginx.conf
-	sed -i '/http {/a\\tlog_format main $remote_addr $remote_user [$time_local] "$request" $status $body_bytes_sent $http_referer "$http_user_agent" "$http_x_forwarded_for";' /etc/nginx/nginx.conf
 	sed -i '/http {/a\\tkeepalive_timeout 7;' /etc/nginx/nginx.conf
 	sed -i '/http {/a\\tcharset utf-8;' /etc/nginx/nginx.conf
 
