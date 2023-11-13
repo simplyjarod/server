2780dab272daee3c07f2cc65f3fc948eda103775
Javier Rodríguez <simplyjarod@gmail.com>
2023-11-13 12:33:05 +0100

    iptables bug fixes

--- iptables-accept-http.sh
+++ iptables-accept-http.sh
@@ -29,22 +29,21 @@ fi
 if [[ $ip = "*" ]]; then
 	echo "All IPs will be allowed to connect through port 80"
 	iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-fi
-
 
 # IPv4 format check:
-if ! [[ $ip =~ ^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$ ]]; then
+elif ! [[ $ip =~ ^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$ ]]; then
 	echo -e "\e[91mThe introduced IP does not seem to be correct.\e[0m"
 	exit 1
+
+else
+	iptables -I INPUT -p tcp -s $ip --dport 80 -j ACCEPT
 fi
 
-
-iptables -I INPUT -p tcp -s $ip --dport 80 -j ACCEPT
 
 if [ -x "$(command -v yum)" ]; then
 	service iptables save
 elif [ -x "$(command -v apt-get)" ]; then
-	/sbin/iptables-save > /etc/iptables/rules.v4
+	iptables-save > /etc/iptables/rules.v4
 else
 	echo -e "\e[91mUnsupported system. Iptables rules were not saved.\e[0m"
 	exit 1
