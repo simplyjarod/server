cdf7392833a40f5579b79c774058cdb61d1dc721
Javier Rodríguez <simplyjarod@gmail.com>
2023-12-01 01:07:06 +0100

    Installation of nodejs updated

--- nodejs-install.sh
+++ nodejs-install.sh
@@ -19,8 +19,17 @@ if [ -x "$(command -v yum)" ]; then
 	yum install -y nodejs
 
 elif [ -x "$(command -v apt-get)" ]; then
-	curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-	apt-get install -y nodejs
+	apt-get install -y ca-certificates curl gnupg
+	mkdir -p /etc/apt/keyrings
+	curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+	NODE_MAJOR=20 # can be changed depending on the version you need
+	apt-get update
+	apt-get install nodejs -y
+
+	# To uninstall:
+	# apt-get purge nodejs
+	# rm -r /etc/apt/sources.list.d/nodesource.list
+	# rm -r /etc/apt/keyrings/nodesource.gpg
 
 else
 	echo -e "\e[91mUnsupported system. Please install NodeJS manually.\e[0m"
